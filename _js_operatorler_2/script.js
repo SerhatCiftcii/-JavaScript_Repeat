622bb022e86f7bf4d4411ee0d0a1c2b39edd494f
Serhat <s.ciftci561@gmail.com>
2025-03-11 02:25:08 +0300

    operatorler bitti

---
+++ _js_operatorler_2/script.js
@@ -0,0 +1,30 @@
+let sonuc;
+let a=20, b=20, c=30;
+//1- aritmetik oparetor
+
+/*sonuc=a+b;
+sonuc=a-b;
+sonuc=a*b;
+sonuc=c*a;
+sonuc=a++;*/
+
+//2-atama operatorler
+
+/*sonuc=a;
+sonuc+= a //sonuc= sonuc+a
+sonuc-=a //sonuc= sonuc-a
+ //sonuc= sonuc+a*/
+
+
+//3- karsılastırma oparetorleri
+
+sonuc = (a==b) //bool
+sonuc = (a!=b)
+sonuc=(3==="3") //tip kontrolude yapar false
+sonuc=(3=="3")//true
+sonuc=(a>=b)
+sonuc=(a<b)
+
+//4-mantıksal operatorler
+
+console.log(sonuc);
