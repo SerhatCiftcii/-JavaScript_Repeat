efd07ca3e07de6133b3277a2e74ab986ec557e87
Serhat <s.ciftci561@gmail.com>
2025-03-11 01:29:09 +0300

    Degiskenler tekrar1

---
+++ _js_kodlama_1/script.js
@@ -0,0 +1,105 @@
+ /*var maasAli= 7000;
+ var maasCan= 7000;
+ var zam=0.3;
+ console.log("maaş Ali: " + maasAli+(500*zam))//maaş ali
+ console.log(maasCan+(500*zam)) //maaş can */
+
+
+
+
+//değişken türleri
+//var alternti let bi fark var if ve for için öğrenmek 
+
+/*var urunAdi="Iphone 13"; //STRİNG
+let urunFiyat=15000; //int
+console.log(typeof urunAdi);
+console.log(typeof urunFiyat);*/
+
+///
+/*let sayi4=15;
+let sayi3=10;
+let sayi1="10";
+let sayi2="20";
+console.log("sayi1+sayi2= "+ sayi1+sayi2);
+console.log("sayi3+sayi4= "+(sayi3+sayi4));
+console.log("sayi1+sayi3 :"+ (sayi3.toString()+sayi1));
+console.log("sayi1+sayi3 :"+ (parseInt(sayi1)+sayi3));*/
+
+///
+/*let sinavNotu=60;
+let basarilimi=(sinavNotu>50);
+if(basarilimi){
+    console.log("basarili");//true 
+}
+else{
+    console.log("basariiiz")//false
+}//*/
+
+/////
+
+/*let yas=10; //10tanımlamazsan undefined olur değer atamamış
+console.log(yas);
+console.log(typeof yas);
+
+let  yas1=""; //boşlık string
+console.log(yas1);
+console.log(typeof yas1);*/
+
+
+// Degiskenler konusu için  uygulama
+
+var isim="ada bilgi";
+var dogumTarihi=2012;
+var Anot=((70+70+80)/3);
+var suankiYas=("Ada bilgi yasıi :" +(2025-dogumTarihi));
+console.log("ADA nOT ORT :"+ Anot)
+console.log();
+console.log();
+
+var isim2="yiğit bilgi";
+var dogumTarihi=2010;
+var Ynot=("Not Ortalamasi Yiğit:" +(40+40+40)/3);
+var suankiYas1=("yiğit bilgi yasi :"+(2025-dogumTarihi));
+console.log("yİĞİT nOT ORT :"+ Ynot)
+console.log();
+
+//50Yİ GEÇERSE BAŞARILI KISITLAMA
+if(Anot>50){
+    console.log("basarili Ada")
+}
+else{
+    console.log("basarısız Ada")
+}
+
+if(Ynot>50){
+    console.log("basarili YİĞİT")
+}
+else{
+    console.log("basarısız YİĞİT")
+}
+
+console.log(suankiYas);
+console.log(suankiYas1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
